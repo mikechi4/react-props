40f9c5693dd21f43d7dbdce222fae9fa4d91101c
Mike Chi <mikechi@MacBook-Pro-4.local>
2019-05-29 21:25:32 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const buttonText = 'Click Me!';
+    return (
+        <div>
+            <label className="label" htmlFor="name"></label>
+            Enter Name:
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color:'white'}}>{buttonText}</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
