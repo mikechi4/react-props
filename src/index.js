46ba4ea10dc2e44651e6bfccba705c4aba18310a
Mike Chi <mikechi@MacBook-Pro-4.local>
2019-06-01 14:47:09 -0400

    finished module

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './commentDetail';
+import faker from 'faker';
+import ApprovalCard from './approvalCard';
 
 const App = () => {
-    const buttonText = 'Click Me!';
     return (
-        <div>
-            <label className="label" htmlFor="name"></label>
-            Enter Name:
-            <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color:'white'}}>{buttonText}</button>
+        <div className="ui container comments">
+            {/* We can wrap child components between parent open and closing tags. this will pass the child in as a prop */}
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:30pm" 
+                    commentText="Nice blog post!" 
+                    avatarImg={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Mike" 
+                    timeAgo="Today at 5:00am" 
+                    commentText="Sa-WEEET d00d!!" 
+                    avatarImg={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 10:15pm" 
+                    commentText="GNAR, bro" 
+                    avatarImg={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            
         </div>
-    )
-}
+    );
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
